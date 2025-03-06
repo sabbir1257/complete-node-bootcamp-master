8b9ccca8388a7862c4c5921f9005f04894c199c9
sabbir1257 <masabbir1257@gmail.com>
2025-03-06 10:54:42 +0600

    Creating a Simple Web Server

--- starter/index.js
+++ starter/index.js
@@ -32,14 +32,19 @@ console.log("Will read file!"); */
 //////////////////////////////////
 /* SERVER */
 const server = http.createServer((req, res) => {
-  const pathName =req.url;
-  
-  if(pathName === '/' || pathName === '/overview'){
-    res.end("This is the OVERVIEW");
-  }else if(pathName === '/product'){
-    res.end('This is the PRODUCT')
-  }
+  const pathName = req.url;
 
+  if (pathName === "/" || pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else {
+    res.writeHead(404, {
+      "content-type": "text/html",
+      'my-own-header': 'hello-world'
+    });
+    res.end("<h1>page not found!</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
